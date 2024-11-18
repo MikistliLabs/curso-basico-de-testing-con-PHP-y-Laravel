90e2f83b52be6fa5a0ce63f9a64bfa2f159b3fad
casc-grillo <cloesanz@gmail.com>
2024-11-18 12:48:42 -0600

    Tests con clase externa, implementación de testeo de funciones de helpers

--- tests/Unit/Helpers/EmailTest.php
+++ tests/Unit/Helpers/EmailTest.php
@@ -7,7 +7,7 @@ use PHPUnit\Framework\TestCase;
 
 class EmailTest extends TestCase
 {
-    public function testEmail(): void
+    public function testEmail()
     {
         $result = Email::validate('i@admin.com');
         $this->assertTrue($result);
